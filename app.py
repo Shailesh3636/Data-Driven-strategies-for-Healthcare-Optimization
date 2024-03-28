582fe47653467ea897b8047ce3c19b85b3128ee7
Shailesh3636 <dsaileshreddy15@gmail.com>
2024-03-28 23:10:49 +0530

    added lungs model and lung page

--- app.py
+++ app.py
@@ -3,7 +3,16 @@ import mysql.connector
 import os
 import pandas as pd
 import pickle
-import numpy as np 
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder, MinMaxScaler, StandardScaler
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import make_pipeline
+import joblib
+
 
 #<-------------------------- Login System ------------------------>
 conn = mysql.connector.connect(host="localhost",user="root",password="",database="healthpluse")
@@ -123,5 +132,111 @@ def predict():
         predicted_disease = get_predicted_value(user_sym)
         desc,pre,med,die,wrkout = recommendation_function(predicted_disease)
     return render_template('Ai.html',predicted_disease = predicted_disease,dis_des=desc,dis_pre=pre[0],dis_med=med,dis_diet=die,dis_workout=wrkout)
+
+#<-------------------------- Heart Prediction ------------------------->
+
+model = joblib.load("models/Heart cardiovascular prediction model.pkl")
+
+@app.route('/loadheart')
+def  heart_load():
+    if 'user_id' in session:
+        return render_template('loadheart.html')
+    else:
+        return redirect('/')
+
+@app.route('/heart')
+def  heart():
+    if 'user_id' in session:
+        return render_template('heart.html')
+    else:
+        return redirect('/')
+
+@app.route("/predictheart", methods=["POST"])
+def predictheart():
+    if request.method == "POST":
+        Fn = request.form["fname"]
+        age = request.form["ag"]
+        mof = "Male"
+        getgen = request.form["ge"]
+        if getgen[0] == "f" or getgen[0] == "F":
+            mof = "Female"
+        ctype = "non-anginal"
+        chestpain = request.form["cp"]
+        if chestpain[1] == "s" or chestpain[1] == "S":
+            ctype = "asymptomatic"
+        elif chestpain[0] == "a" or chestpain[0] == "A":
+            ctype = "atypical angina"
+        elif chestpain[0] == "t" or chestpain[0] == "T":
+            ctype = "typical angina"
+
+        trb = request.form["trest"]
+        ch = request.form["chol"]
+        bfb = 1
+        fbs = request.form["fb"]
+        if fbs[0] == "F" or fbs[0] == "f":
+            bfb = 0
+        re = 0
+        res = request.form["rest"]
+        if res[0] == "l" or res[0] == "L":
+            re = 1
+        elif res[0] == "s" or res[0] == "S":
+            re = 2
+        thalch = request.form["t"]
+        exb = 1
+        exhan = request.form["ex"]
+        if exhan[0] == "F" or exhan[0] == "f":
+            exb = 0
+        dep = request.form["de"]
+        slo = request.form["sl"]
+        sa = 0
+        if slo[0] == "u" or slo[0] == "U":
+            sa = 1
+        elif slo[0] == "d" or slo[0] == "D":
+            sa = 2
+        c = request.form["ca"]
+
+        b = request.form["bm"]
+
+        inputs = [
+            [
+                float(age),
+                mof,
+                ctype,
+                float(trb),
+                float(ch),
+                bfb,
+                float(re),
+                float(thalch),
+                exb,
+                float(dep),
+                float(sa),
+                float(c),
+                float(b),
+            ]
+        ]
+        results = model.predict(inputs)
+        if str(results[0]) == "0":
+            return render_template("noheartdis.html", name=Fn)
+        else:
+            return render_template("heartresult.html", name=Fn, stage=str(results[0]))
+    return render_template("heart.html")
+
+
+#<------------------ Lungs cancer  ------------------>
+
+@app.route('/loadlungs')
+def loadinglungs():
+    if 'user_id' in session:
+        return render_template('loadinglung.html')
+    else:
+        return redirect('/')
+
+@app.route('/lungs')
+def lungs():
+    if 'user_id' in session:
+        return render_template('lung.html')
+    else:
+        return redirect('/')
+    
 if __name__ == "__main__":
     app.run(debug=True)
