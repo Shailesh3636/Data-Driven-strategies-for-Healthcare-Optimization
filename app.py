0fecdb594f6298d3d40977ad9016388ac76f4ffc
Shailesh3636 <dsaileshreddy15@gmail.com>
2024-04-04 14:14:00 +0530

    deployed brain tumor

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session,send_from_directory
 import mysql.connector
 import os
 import pandas as pd
@@ -12,7 +12,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import make_pipeline
 import joblib
-
+import cv2
+import matplotlib.pyplot as plt
+from keras.preprocessing import image
+from keras.models import Sequential
+from keras.layers import Dense
+import os
+from keras.models import model_from_json
+import tensorflow as tf
 
 #<-------------------------- Login System ------------------------>
 conn = mysql.connector.connect(host="sql6.freemysqlhosting.net",user="sql6696569",password="hcSUt3jz3w",database="sql6696569 ")
@@ -347,6 +354,52 @@ def mainkidney():
         return redirect('/')
 
 #<---------------- Brain Tumor ------------------------------>
+
+class_names = ["pituitary_tumor", "no_tumor", "meningioma_tumor", "glioma_tumor"]
+class_names_label = {class_name: i for i, class_name in enumerate(class_names)}
+
+
+UPLOAD_FOLDER = "uploads"
+STATIC_FOLDER = "static"
+json_file = open("model_tumor.json", "r")
+loaded_model_json = json_file.read()
+json_file.close()
+cnn_model = model_from_json(loaded_model_json)
+cnn_model.load_weights("models/model_tumor.h5")
+
+IMAGE_SIZE = 150
+
+
+# Preprocess an image
+def preprocess_image(image):
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [IMAGE_SIZE, IMAGE_SIZE])
+    image /= 255.0  # normalize to [0,1] range
+
+    return image
+
+# Read the image from path and preprocess
+def load_and_preprocess_image(path):
+    image = tf.io.read_file(path)
+
+    return preprocess_image(image)
+
+# Predict & classify image
+def classify(model, image_path):
+
+    preprocessed_imgage = load_and_preprocess_image(image_path)
+    preprocessed_imgage = tf.reshape(
+        preprocessed_imgage, (1, IMAGE_SIZE, IMAGE_SIZE, 3)
+    )
+
+    prob = cnn_model.predict(preprocessed_imgage)
+    # Vector of probabilities
+    pred_labels = np.argmax(prob, axis=1)
+    label = class_names[pred_labels[0]]
+    classified_prob = prob[0][0] if prob[0][0] >= 0.5 else 1 - prob[0][0]
+
+    return label, classified_prob
+
 @app.route('/loadbrain')
 def loadbrain():
     if 'user_id' in session:
@@ -360,5 +413,27 @@ def mainbrain():
         return render_template('brainmain.html')
     else:
         return redirect('/')
+
+@app.route("/classify", methods=["POST", "GET"])
+def upload_file():
+    if 'user_id' in session:
+        if request.method == "GET":
+            return render_template("brainmain.html")
+
+        else:
+            file = request.files["image"]
+            upload_image_path = os.path.join(UPLOAD_FOLDER, file.filename)
+            print(upload_image_path)
+            file.save(upload_image_path)
+            label, prob = classify(cnn_model, upload_image_path)
+            prob = round((prob * 100), 2)
+        return render_template("classify.html", image_file_name=file.filename, label=label, result=prob)
+    else:
+        return redirect('/')
+    
+@app.route("/classify/<filename>")
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
 if __name__ == "__main__":
     app.run(debug=True)
